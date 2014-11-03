3425052714293eb83392d2ddd2a0834bb987194f
Melanie Sumner <melaniersumner@gmail.com>
2014-11-03 15:31:21 -0500

    Create thankyou.php

---
+++ thankyou.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Tell Me About Yourself</title>
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+    </head>
+  </head>
+
+  <body>
+   <div class="container">
+    <h1 class="page-header text-center">Thank you!</h1>
+    <p class="text-center">Thank you! Someone will be in touch soon.</p>
+    <div class="row">
+    <div class="col-xs-12 col-sm-4 col-sm-offset-4">
+    <p class="text-center">
+    <img src="assets/images/robot12.png" class="img-responsive" alt=""/>
+    </p>
+    </div>
+    </div>
+  </div>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
